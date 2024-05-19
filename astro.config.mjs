92e159b8c19550ceca3582365baad6af492dc438
Ashfid <ashfidable.islam@gmail.com>
2024-05-19 04:13:47 +0300

    feat: deploy github action

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon()]
+  site: "https://thefireKS.github.io",
+  integrations: [tailwind(), icon()],
 });
